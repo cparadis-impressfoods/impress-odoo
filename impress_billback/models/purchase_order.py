dc497d7724b3489250aa572ebf023707fc14c351
cparadis-impressfoods <cparadis@impressfoods.com>
2024-12-10 10:37:15 -0500

    Merging impress_billback into main (#3)
    * Added some comments to ir_sequence.py
    
    * Added impress-barcode
    
    * Added origin to stock_picking kanban view
    
    * Added line break when origin is not set in impress_barcode kanban view
    
    * Added online sale label
    
    * Added online_sale_labels to manifest
    
    * Changed impress_cstock_customizations manifest
    
    * impress_stock_customizations: removed actions from manifest
    
    * changed action_report_online_sale_label report_name and report_file
    
    * Correct typo in report_online_sale_label
    
    * Added licences to all modules
    
    * Added add_barcode to stock_delivery_customizations
    
    * Added ability to add quality check on demand for a workorder. Currently, the newly created quality check does not appear in the shop floor view of the workorder
    
    * Removed test_patching_shop_floor
    
    * Added auto-install to impress_qc_on_demand
    
    * Fixed auto-install dependencies
    
    * Added impress production billing
    
    * Changed manifest for impress_production_billing
    
    * Added icon to impress_production_billing
    
    * Added error handling and removed automatic recalculation of the sale order line ordered quantity
    
    * Added green decoration when SO customer ref is valid and found a corresponding SO
    
    * Removed logging from impress_production_billing
    
    * Merge branch 'main' into dev
    
    * Replaced impress_accounting_customizations for impres_billback

---
+++ impress_billback/models/purchase_order.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    billback_invoice_ids = fields.Many2many('account.move', compute='_compute_billback_invoice_ids')
+    billback_invoice_count = fields.Integer(compute='_compute_billback_invoice_ids')
+
+
+    def _compute_billback_invoice_ids(self):
+        self.billback_invoice_ids = self.invoice_ids.filtered(lambda inv: inv.billback_invoice_id).mapped(lambda inv: inv.billback_invoice_id.id)
+        self.billback_invoice_count = len(self.billback_invoice_ids)
+
+    def action_view_billback_invoice(self, invoices=False):
+        """This function returns an action that display existing billback bills of
+        given purchase order ids. When only one found, show the billback bill
+        immediately.
+        """
+        if not invoices:
+            self.invalidate_model(['billback_invoice_ids'])
+            invoices = self.billback_invoice_ids
+
+        result = self.env['ir.actions.act_window']._for_xml_id('account.action_move_in_invoice_type')
+        # choose the view_mode accordingly
+        if len(invoices) > 1:
+            result['domain'] = [('id', 'in', invoices.ids)]
+        elif len(invoices) == 1:
+            res = self.env.ref('account.view_move_form', False)
+            form_view = [(res and res.id or False, 'form')]
+            if 'views' in result:
+                result['views'] = form_view + [(state, view) for state, view in result['views'] if view != 'form']
+            else:
+                result['views'] = form_view
+            result['res_id'] = invoices.id
+        else:
+            result = {'type': 'ir.actions.act_window_close'}
+
+        return result

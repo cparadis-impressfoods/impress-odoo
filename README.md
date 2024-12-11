273e25e27d0892ce37456e7260bad079429ff45b
Cédric Paradis <cparadis@impressfoods.com>
2024-12-11 11:37:43 -0500

    Added documents_archive

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # impress-odoo
 Modules developed by Impress Foods SEC for internal usage.
-This repo contains each addons as a separate submodule.
---
+++ documents_archive/models/documents_document.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class DocumentsDocument(models.Model):
+    _inherit = 'documents.document'
+
+    archived = fields.Boolean('Archived', default=False)
+
+
+    def is_archived(self):
+        return self.archived
+
+    def action_soft_archive(self):
+        if not self.archived: 
+            self.archived = True
+        if self.attachment_id:
+            matching_attachment = self._get_matching_attachment()
+            if matching_attachment:
+                matching_attachment.active = False
+
+    def action_soft_unarchive(self):
+        if self.archived:
+            self.archived = False
+        if self.attachment_id:
+            matching_attachment = self._get_matching_attachment()
+            if matching_attachment:
+                matching_attachment.active = True
+
+    def _get_matching_attachment(self):
+        records = self.env['product.document'].search([('ir_attachment_id', '=', self.attachment_id.id),('active', 'in', [True, False])])
+        _logger.warning("Found matching attachments: %s", records)
+        if records:
+            return records[0]
+        else:
+            return None

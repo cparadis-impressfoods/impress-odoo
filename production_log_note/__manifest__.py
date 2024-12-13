6c6ffc92928c7d4291f75265e4275e00a1c789f0
Cédric Paradis <cparadis@impressfoods.com>
2024-12-13 10:30:09 -0500

    Add production_log_note

---
+++ production_log_note/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Production_log_note',
+    'version': '17.0.1.0.0',
+    'summary': """ Backport of V18 feature where a note can be added to a production order""",
+    'author': 'Cédric Paradis',
+    'website': '',
+    'category': 'Hidden',
+    'depends': ['base', 'web', 'mrp_workorder'],
+    "data": [
+        "views/mrp_production_views.xml"
+    ],'assets': {
+              'web.assets_backend': [
+                  'production_log_note/static/src/**/*'
+              ],
+          },
+
+    'installable': True,
+
+    'license': 'GPL-2',
+}
---
+++ production_log_note/static/src/mrp_display_record.js
@@ -0,0 +1,19 @@
+/** @odoo-module **/
+
+import {patch} from '@web/core/utils/patch';
+import {MrpDisplayRecord} from '@mrp_workorder/mrp_display/mrp_display_record';
+import {MrpLogNoteDialog} from './dialog/mrp_log_note_dialog';
+import { _t } from "@web/core/l10n/translation";
+
+patch(MrpDisplayRecord.prototype, {
+
+    editLogNote() {
+        const title = _t("Log Note");
+        const reload = () => this.env.reload();
+        const params = { body: '', record: this.props.production, reload, title};
+        this.dialog.add(MrpLogNoteDialog, params);
+    },
+    get logNote() {
+        return this.props.production.data.log_note;
+    },
+});

2a994fd87916b272c2f9112d4ab3daef3cbb3b99
Cédric Paradis <cparadis@impressfoods.com>
2025-01-16 15:49:03 -0500

    Removed unused move.css file from impress_barcode

--- impress_barcode/models/stock_picking.py
+++ impress_barcode/models/stock_picking.py
@@ -16,13 +16,13 @@ class StockPicking(models.Model):
         products = self.move_ids.product_id
 
         # Add all stock.move from picking to the cache
-        data['records']['stock.move'] = (moves.read(moves._get_fields_stock_barcode(), load=False))
+        data['records']['stock.move'] = (moves.read(moves._get_fields_stock_barcode(), load='None'))
         
         # To allow for further overrides, do not overwrite the records sent to cache.
         # Instead, add the missing ones by taking all products in the stock.move and add
         # only add those not already present
         data['records']['product.product'].extend(
-            [i for i in products.read(products._get_fields_stock_barcode(), load=False) 
+            [i for i in products.read(products._get_fields_stock_barcode(), load='None') 
                 if i['id'] not in [j['id'] for j in data['records']['product.product']]]
             )
 

042b6f6394009df462b068fe0344fa0200417848
cparadis-impressfoods <cparadis@impressfoods.com>
2025-02-03 14:12:27 -0500

    Dev (#17)
    * removed unused view
    
    * Added translations to impress_lot_lab
    
    * Corrected auto-formatting that broke module
    
    * Changed view names to reflect module change for lot lab
    
    * Updated impress_lot_lab
    
    * Added smart button to sale order allowing to see linked manufacturing orders
    
    * changed view name in production_billing
    
    * Added impress_prevent_workorder_bo
    
    * Added ignore for actions to remove pylance errors
    
    * re-enabled barcode customization
    
    * Added multiple modules: quality_logs, quality_worksheets, consume_maintenance and deposit
    
    * renamed worksheets to match current date

--- impress_quality_worksheets/__manifest__.py
+++ impress_quality_worksheets/__manifest__.py
@@ -7,14 +7,15 @@
     'category': ' Hidden',
     'depends': ['base', 'impress_quality_logs', 'worksheet', 'quality_control_worksheet'],
     'data': [
-        'data/LOMA/LOMAWorksheet_20241202.xml',
-        'views/LOMA/LOMAWorksheet_20241202.xml',
-        'data/HPP/HPPWorksheet_20241202.xml',
-        'views/HPP/HPPWorksheet_20241202.xml',
-        'data/metal/MetalWorksheet_20241204.xml',
-        'views/metal/MetalWorksheet_20241204.xml',
-        'data/coding/CodingWorksheet_20241204.xml',
-        'views/coding/CodingWorksheet_20241204.xml',
+        'data/LOMA/LOMAWorksheet_20250203.xml',
+        'views/LOMA/LOMAWorksheet_20250203.xml',
+        'data/HPP/HPPWorksheet_20250203.xml',
+        'views/HPP/HPPWorksheet_20250203.xml',
+        'data/metal/MetalWorksheet_20250203.xml',
+        'views/metal/MetalWorksheet_20250203.xml',
+        'data/coding/CodingWorksheet_20250203.xml',
+        'views/coding/CodingWorksheet_20250203.xml',
+
     ],
 
     'installable': True,

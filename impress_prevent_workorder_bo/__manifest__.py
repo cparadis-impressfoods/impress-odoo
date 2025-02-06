e5b665f569a0e8207087663bee3588f3f482f7f6
Cédric Paradis <cparadis@impressfoods.com>
2025-02-06 08:30:45 -0500

    Updated some module versions

--- impress_prevent_workorder_bo/__manifest__.py
+++ impress_prevent_workorder_bo/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Impress_prevent_workorder_bo',
-    'version': '0.0.1',
+    'version': '0.1.0',
     'summary': """ Prevent BO on on Workorders""",
     'description': """ Prevents the creation of BO on workorder validation when producing less than expected.""",
     'author': 'Cédric Paradis',

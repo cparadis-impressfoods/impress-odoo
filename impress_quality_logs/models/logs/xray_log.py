a80eeb00775c836a4b7f2ace9b2e9a2c97a4a4b0
Cédric Paradis <cparadis@impressfoods.com>
2025-02-11 12:52:03 -0500

    Added x-ray log and x-ray log line + worksheets

---
+++ impress_quality_logs/models/logs/xray_log.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class Xray_log(models.Model):
+    _name="x_ray.log"
+    _inherit="log.mixin"
+    _description = 'X-Ray Detector Log'
+
+    log_line_ids = fields.One2many(comodel_name='x_ray.log.line', inverse_name='x_ray_log_id')
+
+    average_threshold = fields.Integer('Average Threshold')
+
+    def action_view_x_ray_lines(self):
+        self.ensure_one()
+        action = {
+            'res_model': 'x_ray.log.line',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'list,form',
+            'domain': [('x_ray_log_id', '=', self.id)],
+        }
+        return action

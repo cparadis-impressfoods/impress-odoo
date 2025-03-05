114338ded075d071442cb954c1f4dfd11dd0a32f
Cédric Paradis <cparadis@impressfoods.com>
2025-03-05 12:39:26 -0500

    Corrected sale_order gantt views

--- impress_sales_customizations/__manifest__.py
+++ impress_sales_customizations/__manifest__.py
@@ -9,8 +9,8 @@
     """,
     'license': 'GPL-2',
     # data files always loaded at installation
-    'data': [
-        'views/impress_sales_customizations_sales_order_views.xml',
+    "data": [
+        "views/sale_order_views.xml"
     ],
     # data files containing optionally loaded demonstration data
     'demo': [

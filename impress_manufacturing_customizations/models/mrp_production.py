d8403ff255c017eb9351ad8803bd0e1b2e6f07fc
Cédric Paradis <cparadis@impressfoods.com>
2025-03-06 09:39:19 -0500

    Added extra field on mrp_production to allow filtering by product category

--- impress_manufacturing_customizations/models/mrp_production.py
+++ impress_manufacturing_customizations/models/mrp_production.py
@@ -11,3 +11,5 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     sequence = fields.Integer('Sequence')
+    product_category_id = fields.Many2one('product.category', 'Product Category', 
+                                            related="product_tmpl_id.categ_id", store=True, depends=["product_tmpl_id"])

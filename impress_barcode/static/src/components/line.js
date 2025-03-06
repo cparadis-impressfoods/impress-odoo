237b422204b3f71801c33ef58fd7e4c48aa8669e
Cédric Paradis <cparadis@impressfoods.com>
2025-03-06 12:39:35 -0500

    Formatting changes

--- impress_barcode/static/src/components/line.js
+++ impress_barcode/static/src/components/line.js
@@ -8,11 +8,7 @@ patch(LineComponent.prototype, {
 
     get totalDemand() {
         if (this.line.ids) {
-        // We need to calculate the full quantity for grouped lines
-           
-        //    const move_ids = this.groupedLines[0].lines.map((x) => x.move_id);
-        //    const quantities = move_ids.map((x) => this.cache.getRecord('stock.move', x)).map((y) => y.product_uom_qty);
-        //    const total_quantity = quantities.reduce((acc, currentVal) => acc + currentVal, 0);
+            // We need to calculate the full quantity for grouped lines
             const move_ids = this.line.lines.map((x) => x.move_id).filter((e, i, self) => i === self.indexOf(e));
             const quantities = move_ids.map((x) => this.env.model.cache.getRecord('stock.move', x)).map((y) => y.product_uom_qty)
             const total_quantity = quantities.reduce((acc, currentVal) => acc + currentVal, 0);

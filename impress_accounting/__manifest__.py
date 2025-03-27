fb8a3d90be06207ddca6dc4bb50937d68b70eae7
Cédric Paradis <cparadis@impressfoods.com>
2025-03-27 14:24:45 -0400

    Ajout impress_accounting

---
+++ impress_accounting/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Impress_accounting',
+    'version': '17.0.0.0.1',
+    'summary': """ Impress_accounting Summary """,
+    'author': 'Cédric Paradis',
+    'website': '',
+    'category': 'Hidden',
+    'depends': ['base','account' ],
+    "data": [
+        "views/res_partner_views.xml"
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ impress_accounting/models/res_partner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    invoice_partner_id = fields.Many2one('res.partner', string='Invoice Partner')
+
+
+    def get_invoicing_partner(self):
+        if self.invoice_partner_id:
+            return self.invoice_partner_id.id
+        else:
+            return self.id

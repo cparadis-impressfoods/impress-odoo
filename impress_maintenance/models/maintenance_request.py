f95de374ee2d6de04f0a73f4f5d1149e6f66b4b5
Cédric Paradis <cparadis@impressfoods.com>
2025-04-03 12:31:26 -0400

    Change field string and added security group for impress_maintenance

--- impress_maintenance/models/maintenance_request.py
+++ impress_maintenance/models/maintenance_request.py
@@ -10,4 +10,8 @@ class MaintenanceRequest(models.Model):
     _inherit = "maintenance.request"
 
     notes = fields.Text(string="Notes")
-    technician_id = fields.Many2one("hr.employee", string="Technician")
+    technician_id = fields.Many2one("hr.employee", string="Assigned Employee")
+
+    def create(self, vals_list):
+        _logger.warning(vals_list)
+        return super(MaintenanceRequest, self).create(vals_list)

6f3a7da7dde26c77c0dbd9239f8e5c8012a54670
Cédric Paradis <cparadis@impressfoods.com>
2025-04-04 10:59:00 -0400

    Removed uneeded action in production_log_note

--- production_log_note/models/mrp_production.py
+++ production_log_note/models/mrp_production.py
@@ -11,22 +11,3 @@ class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
     log_note = fields.Text(string="Log Note")
-
-    def action_log_note(self):
-        self.ensure_one()
-        return {
-            "type": "ir.actions.act_window",
-            "res_id": self.id,
-            "res_model": "mrp.production",
-            "views": [
-                [
-                    self.env.ref("mrp_workorder.mrp_production_view_form_log_note").id,
-                    "form",
-                ]
-            ],
-            "name": _("Add log note"),
-            "target": "new",
-            "context": {
-                "default_production_id": self.id,
-            },
-        }

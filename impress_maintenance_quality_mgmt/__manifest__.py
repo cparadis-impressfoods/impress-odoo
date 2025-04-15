a19071c29796d8a76a1f74f0402eaefd0800ab78
Cédric Paradis <cparadis@impressfoods.com>
2025-04-15 14:49:27 -0400

    removed pre-commit

--- impress_maintenance_quality_mgmt/__manifest__.py
+++ impress_maintenance_quality_mgmt/__manifest__.py
@@ -2,16 +2,12 @@
     "name": "Impress Maintenance Quality Management",
     "version": "17.0.1.0.0",
     "summary": """ Impress Foods quality management for maintenance """,
-    "description": """
-    Adds functionalities to segregate maintenance by departement (Maintenance vs Quality Control)
-    """,
     "author": "Cédric Paradis",
     "website": "",
     "category": "Maintenance",
     "depends": ["base", "base_maintenance", "quality", "maintenance_worksheet"],
     "data": [
         "data/team_tags.xml",
-        "security/maintenance.xml",
         "security/ir.model.access.csv",
         "views/maintenance_equipment_category_views.xml",
         "views/maintenance_team_tag_views.xml",
